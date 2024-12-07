9ed856ec7636bdf6ff3a17daa79973d950317649
Ian Parra <ianparra200@gmail.com>
2024-12-07 16:57:16 -0300

    feat: uprising settings

--- src/routes/channel/[id]/+page.svelte
+++ src/routes/channel/[id]/+page.svelte
@@ -4,8 +4,12 @@
     let { data } = $props();
 
     let channel = $derived(getChannel(data.id));
+    $effect(() => {
+        if (channel) {  
+            document.title = `#${channel.name} | Uprising demo`;
+        }
+    });
 </script>
-
 {#if channel}
     <ChannelArea {channel} />
 {/if}
