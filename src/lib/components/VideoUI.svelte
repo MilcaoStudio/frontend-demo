af0f235ffa57364f1d1fb1b1eef9627da44b68ca
Ian Parra <ianparra200@gmail.com>
2025-06-02 00:02:11 -0400

    feat: show participant streams

--- src/lib/components/VideoUI.svelte
+++ src/lib/components/VideoUI.svelte
@@ -1,60 +1,80 @@
 <script lang="ts">
-    import { voiceState } from "$lib/voice/VoiceState";
-    import { getContext } from "svelte";
-    import MicAction from "./actions/MicAction.svelte";
-    import Avatar from "./Avatar.svelte";
-    import VideoTrack from "./VideoTrack.svelte";
-    import type { User } from "uprising.js";
-    import VideoAction from "./actions/VideoAction.svelte";
-    import ScreencastAction from "./actions/ScreencastAction.svelte";
+  import { voiceState } from "$lib/voice/VoiceState";
+  import { getContext } from "svelte";
+  import MicAction from "./actions/MicAction.svelte";
+  import Avatar from "./Avatar.svelte";
+  import VideoTrack from "./VideoTrack.svelte";
+  import { User } from "uprising.js";
+  import VideoAction from "./actions/VideoAction.svelte";
+  import ScreencastAction from "./actions/ScreencastAction.svelte";
 
-    //let stream = voiceState.stream;
-    let streams = voiceState.streams;
-    $: callStream = streams.get("user");
-    $: displayStream = streams.get("display");
-    let user = getContext<User>("user");
+  //let stream = voiceState.stream;
+  let streams = voiceState.streams;
+  let participants = $state(voiceState.participants);
+  let users = $derived.by(() =>{
+    const keys = [...participants.keys()];
+    console.debug("Participants (ids)", keys);
+    return new Map(
+      keys.map((id) => [
+        id,
+        User.create({ id, username: id }),
+      ])
+    )}
+  );
+  
+  let localVideoStream = $state(streams.get("user"));
+  let localDisplayStream = $state(streams.get("display"));
+  let user = getContext<User>("user");
 </script>
 
 <div class="displayContainer">
+  <div class="row">
+    <Avatar size={80} target={user} />
+    {#if localVideoStream}
+      {#each localVideoStream.getVideoTracks() as track (track.id)}
+        <VideoTrack stream={localVideoStream} />
+      {/each}
+    {/if}
+    {#if localDisplayStream}
+      {#each localDisplayStream.getVideoTracks() as track (track.id)}
+        <VideoTrack stream={localDisplayStream} />
+      {/each}
+    {/if}
+  </div>
+  {#each participants.entries() as [id, data] (id)}
     <div class="row">
-        <Avatar size={80} target={user} />
-        {#if callStream}
-            {#each callStream.getVideoTracks() as track (track.id)}
-                <VideoTrack stream={callStream} />
-            {/each}
-        {/if}
-        {#if displayStream}
-            {#each displayStream.getVideoTracks() as track (track.id)}
-                <VideoTrack stream={displayStream} />
-            {/each}
-        {/if}
+        <Avatar size={80} target={users.get(id)} />
+        {#each data.streams as stream (stream.id)}
+            <VideoTrack stream={stream} />
+        {/each}
     </div>
+  {/each}
 
-    <!-- TODO: View remote streams -->
-    <div class="actions">
-        <MicAction size={32} />
-        <VideoAction size={32} />
-        <ScreencastAction size={32} />
-    </div>
+  <!-- TODO: View remote streams -->
+  <div class="actions">
+    <MicAction size={32} />
+    <VideoAction size={32} />
+    <ScreencastAction size={32} />
+  </div>
 </div>
 
 <style>
-    .displayContainer {
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-        padding: 20px 0;
-        background: var(--bgHighlight);
-    }
-    .row {
-        display: flex;
-        gap: 16px;
-        align-items: center;
-        justify-content: center;
-    }
-    .actions {
-        display: flex;
-        gap: 32px;
-        justify-content: center;
-    }
+  .displayContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding: 20px 0;
+    background: var(--bgHighlight);
+  }
+  .row {
+    display: flex;
+    gap: 16px;
+    align-items: center;
+    justify-content: center;
+  }
+  .actions {
+    display: flex;
+    gap: 32px;
+    justify-content: center;
+  }
 </style>
