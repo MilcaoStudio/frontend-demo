dd9cf912dad154f2782d9260087f6ebdec321bbc
Ian Parra <ianparra200@gmail.com>
2025-06-02 01:32:56 -0400

    feat: interactive button for video track

--- src/lib/components/VideoTrack.svelte
+++ src/lib/components/VideoTrack.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
     let {stream}: {stream: MediaStream} = $props();
-    let ref: HTMLVideoElement;
-    
+    let ref: HTMLVideoElement|undefined;
+
     $effect(()=>{
-        ref.srcObject = stream;
+        console.debug(stream);
+        if (ref) ref.srcObject = stream;
     });
+
+    function onclick() {
+        console.debug(stream);
+    }
 </script>
 
-<!-- svelte-ignore a11y_media_has_caption -->
-<video autoplay bind:this={ref}></video>
+<button class="control" onclick={onclick}>
+    <!-- svelte-ignore a11y_media_has_caption -->
+    <video autoplay bind:this={ref}></video>
+</button>
+
 
 <style>
+    .control {
+        background: transparent;
+        border: unset;
+        cursor: pointer;
+    }
     video {
         width: 240px;
         height: 135px;
